aad98bc2de7723d3dbfa49962b60d6c72fca7b36
ayron1705 <34888607+ayron1705@users.noreply.github.com>
2021-08-17 21:47:27 +0300

    fix

---
+++ script/js.js
@@ -0,0 +1,45 @@
+menu.onclick = function myFunction() {
+	var x = document.getElementById('myTopnav');
+
+	if(x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
+}
+
+/*появление заголовка */
+const animItems = document.querySelectorAll('._anim-items');
+
+if(animItems.length > 0) {
+	window.addEventListener('scroll', animOnScroll);
+	function animOnScroll() {
+		for (let index = 0; index < animItems.length; index++) {
+			const animItem = animItems[index];
+			const animItemHeight = animItem.offsetHeight;
+			const animItemOffset = offset(animItem).top;
+			const animStart = 4;
+
+			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+			if (animItemHeight > window.innerHeight) {
+				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			}
+
+			if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+				animItem.classList.add('_active');
+			} else {
+				animItem.classList.remove('_active');
+			}
+		}
+	}
+	function offset(el) {
+		const rect = el.getBoundingClientRect(),
+			scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		return{
+			top: rect.top + scrollTop, left: rect.left + scrollLeft
+		}
+	}
+	animOnScroll();
+}
